ae806eed6ed18852e0060fa921437752e6847a65
guilhermeoliveira-devjourneylog <dev_guilherme_oliveira@outlook.com>
2024-02-18 22:19:50 +0000

    Update main.py

--- ProgrammingAssignment/ProgrammingAssignment/algorithms/main.py
+++ ProgrammingAssignment/ProgrammingAssignment/algorithms/main.py
@@ -5,20 +5,20 @@ def main():
     print("3. Kosaraju")
     print("4. Quick Sort")
 
+    algorithms = {
+        '1': ('dijkstra', 'run'),
+        '2': ('karger_min_cut', 'run'),
+        '3': ('kosaraju', 'run'),
+        '4': ('quick_sort', 'run')
+    }
+
     choice = input("Enter the number of the algorithm: ")
 
-    if choice == '1':
-        import dijkstra
-        dijkstra.run()
-    elif choice == '2':
-        import karger_min_cut
-        karger_min_cut.run()
-    elif choice == '3':
-        import kosaraju
-        kosaraju.run()
-    elif choice == '4':
-        import quick_sort
-        quick_sort.run()
+    if choice in algorithms:
+        module_name, function_name = algorithms[choice]
+        module = __import__(module_name)
+        function = getattr(module, function_name)
+        function()
     else:
         print("Invalid option. Please select a number from 1 to 4.")
 
